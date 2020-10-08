b8aca8fd70d5465307666f8004acbccab4227c06
chaitanyakasaraneni <chaitanyakrishna755@gmail.com>
2020-10-08 15:19:13 -0700

    test: checking CI/CD pipeline

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(name='simple_kNN',
-      version='0.1',
+      version='0.4',
       description='Simple kNN algorithm with k-Fold Cross Validation',
       author='Chaitanya Krishna Kasaraneni',
       author_email='kc.kasaraneni@gmail.com',
+      long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chaitanyakasaraneni/simple-kNN",
       packages=setuptools.find_packages(),
       classifiers=[
         "Programming Language :: Python :: 3",
