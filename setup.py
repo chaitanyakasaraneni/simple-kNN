86d09be8003fa2c621c78d52d9ce54d92e3148a9
chaitanyakasaraneni <chaitanyakrishna755@gmail.com>
2020-10-08 18:11:00 -0700

    new: checks for version tags

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
+from pathlib import Path
+import re
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+if __name__ == "__main__":
 
-setuptools.setup(name='simple_kNN',
-      version='0.5',
-      description='Simple kNN algorithm with k-Fold Cross Validation',
-      author='Chaitanya Krishna Kasaraneni',
-      author_email='kc.kasaraneni@gmail.com',
-      long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/chaitanyakasaraneni/simple-kNN",
-      packages=setuptools.find_packages(),
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.6',
-        zip_safe=False)
+    # Read description from README
+    with Path(Path(__file__).parent, "README.md").open(encoding="utf-8") as file:
+        long_description = file.read()
+        
+    # Read metadata from version.py
+    with Path("simple_kNN/version.py").open(encoding="utf-8") as file:
+        metadata = dict(re.findall(r'__([a-z]+)__\s*=\s*"([^"]+)"', file.read()))
+
+    setuptools.setup(name='simple_kNN',
+            version=metadata["version"],
+            author=metadata["author"],
+            author_email='kc.kasaraneni@gmail.com',
+            description='Simple kNN algorithm with k-Fold Cross Validation',
+            install_requires=["numpy", "pandas", "operator", "scikit-learn","random"],
+            long_description=long_description,
+            long_description_content_type="text/markdown",
+            url="https://github.com/chaitanyakasaraneni/simple-kNN",
+            packages=setuptools.find_packages(),
+            classifiers=[
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 3.6",
+                "Intended Audience :: Science/Research",
+            ],
+            python_requires='>=3.6',
+            zip_safe=False)
---
+++ simple_kNN/version.py
@@ -0,0 +1,3 @@
+
+__version__ = "1.0.1"
+__author__ = "Chaitanya Krishna Kasaraneni"
