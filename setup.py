5027bbed3713910c8a2f5ebc6846b51e3fcfefc5
chaitanyakasaraneni <chaitanyakrishna755@gmail.com>
2020-10-11 01:40:46 -0700

    fix

--- setup.py
+++ setup.py
@@ -21,9 +21,8 @@ if __name__ == "__main__":
             long_description=long_description,
             long_description_content_type="text/markdown",
             url="https://github.com/chaitanyakasaraneni/simple-kNN",
-            packages=['simple_kNN'],
-            package_dir={'simple_kNN':'simple_kNN'},
-            package_data={'simple_kNN': ['dataset/data/*.csv', 'dataset/data/*.csv.gz']},
+            packages=setuptools.find_packages(),
+            include_package_data=True,
             classifiers=[
                 "License :: OSI Approved :: MIT License",
                 "Operating System :: OS Independent",
